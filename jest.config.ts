fd6d1d71c67105368a08d2df1d10eb1ed84c3e55
shimisnow <14943507+shimisnow@users.noreply.github.com>
2023-05-11 17:21:36 -0300

    feat: auth service initial setup

--- jest.config.ts
+++ jest.config.ts
@@ -1,15 +1,5 @@
-/* eslint-disable */
+import { getJestProjects } from '@nrwl/jest';
+
 export default {
-  displayName: 'poc-nestjs',
-  preset: './jest.preset.js',
-  testEnvironment: 'node',
-  transform: {
-    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }]
-  },
-  moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: './coverage/poc-nestjs',
-  testMatch: [
-    '<rootDir>/src/**/__tests__/**/*.[jt]s?(x)',
-    '<rootDir>/src/**/*(*.)@(spec|test).[jt]s?(x)',
-  ],
+  projects: getJestProjects(),
 };
