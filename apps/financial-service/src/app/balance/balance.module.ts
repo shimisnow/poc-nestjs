0b27d19e6574d209442f5b29578b302fd72c8c21
shimisnow <14943507+shimisnow@users.noreply.github.com>
2023-10-03 20:31:37 -0300

    feat: fix in jwt declaration

--- apps/financial-service/src/app/balance/balance.module.ts
+++ apps/financial-service/src/app/balance/balance.module.ts
@@ -2,18 +2,10 @@ import { Module } from '@nestjs/common';
 import { BalanceService } from './balance.service';
 import { BalanceController } from './balance.controller';
 import { BalancesRepositoryModule } from './repositories/balances-repository.module';
-import { JwtModule } from '@nestjs/jwt';
 import { AuthorizationModule } from '../authorization/authorization.module';
 
 @Module({
-  imports: [
-    BalancesRepositoryModule,
-    AuthorizationModule,
-    JwtModule.register({
-      global: true,
-      secret: process.env.JWT_SECRET_KEY,
-    }),
-  ],
+  imports: [BalancesRepositoryModule, AuthorizationModule],
   providers: [BalanceService],
   controllers: [BalanceController],
   exports: [BalanceService],
--- apps/financial-service/src/app/transaction/transaction.module.ts
+++ apps/financial-service/src/app/transaction/transaction.module.ts
@@ -4,18 +4,9 @@ import { TransactionService } from './transaction.service';
 import { TransactionsRepositoryModule } from './repositories/transactions-repository.module';
 import { BalanceModule } from '../balance/balance.module';
 import { AuthorizationModule } from '../authorization/authorization.module';
-import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [
-    TransactionsRepositoryModule,
-    BalanceModule,
-    AuthorizationModule,
-    JwtModule.register({
-      global: true,
-      secret: process.env.JWT_SECRET_KEY,
-    }),
-  ],
+  imports: [TransactionsRepositoryModule, BalanceModule, AuthorizationModule],
   controllers: [TransactionController],
   providers: [TransactionService],
 })
