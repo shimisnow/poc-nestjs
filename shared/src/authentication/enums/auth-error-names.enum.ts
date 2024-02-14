adbaeecb720a0887d8f42a68cc5a03e464850ac6
shimisnow <14943507+shimisnow@users.noreply.github.com>
2024-02-14 10:36:05 -0300

    feat(#56): new enum

---
+++ shared/src/authentication/enums/auth-error-names.enum.ts
@@ -0,0 +1,15 @@
+/**
+ * Error names to be used at the name field of an UnauthorizedException
+ */
+export enum AuthErrorNames {
+  JWT_EMPTY_ERROR = 'EmptyJsonWebTokenError',
+  JWT_ERROR = 'JsonWebTokenError',
+  JWT_PAYLOAD_STRUCTURE_ERROR = 'JsonWebTokenPayloadStrutureError',
+  JWT_EXPIRED_ERROR = 'TokenExpiredError',
+
+  /** When the token was invalidated by logout or password change */
+  JWT_INVALIDATED_BY_SERVER = 'TokenInvalidatedByServer',
+
+  /** User does not exists, is inactive or password is incorrect */
+  CREDENTIAL_ERROR = 'CredentialError'
+}
