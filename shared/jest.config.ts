3e3e020fec9333995d999e54d445ecf7fd207dd1
shimisnow <14943507+shimisnow@users.noreply.github.com>
2024-02-15 09:02:07 -0300

    test: jest setup for shared libraries

--- shared/jest.config.ts
+++ shared/jest.config.ts
@@ -6,6 +6,18 @@ export default {
   transform: {
     '^.+\\.[tj]sx?$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  moduleFileExtensions: ['ts', 'js', 'html'],
   coverageDirectory: '../coverage/shared',
+  collectCoverageFrom: ['<rootDir>/src/**'],
+  /* coveragePathIgnorePatterns: [
+    'main.ts',
+    'repl.ts',
+    '.*\.(module|dto|serializer|mock)\.ts$'
+  ], */
+  coverageReporters: ['clover', 'html'],
+  /* coverageThreshold: {
+    global: {
+      lines: 60,
+    },
+  }, */
 };
