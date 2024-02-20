f7f82565c212d35b0b4a819211b7be3528646683
shimisnow <14943507+shimisnow@users.noreply.github.com>
2024-02-20 16:33:47 -0300

    test(#73): integration test for createDebitTransaction() function

--- apps/financial-service/src/app/transaction/transaction.service.spec.ts
+++ apps/financial-service/src/app/transaction/transaction.service.spec.ts
@@ -2,16 +2,15 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable no-case-declarations */
 import { Test, TestingModule } from '@nestjs/testing';
-import { TransactionService } from './transaction.service';
 import { CACHE_MANAGER } from '@nestjs/cache-manager';
-import { getRepositoryToken } from '@nestjs/typeorm';
-import { TransactionEntity } from '@shared/database/financial/entities/transaction.entity';
-import { TransactionsRepository } from './repositories/transactions.repository';
+import { TransactionService } from './transaction.service';
+import { TransactionsRepository } from './repositories/transactions/transactions.repository';
 import { BalanceService } from '../balance/balance.service';
-import { BalancesRepository } from '../balance/repositories/balances.repository';
-import { UserService } from '../user/user.service';
-import { ForbiddenException, PreconditionFailedException } from '@nestjs/common';
+import { AccountsRepository } from './repositories/accounts/accounts.repository';
 import { TransactionTypeEnum } from '@shared/database/financial/enums/transaction-type.enum';
+import { ForbiddenException, PreconditionFailedException } from '@nestjs/common';
+import { CreatePairTransactionBody } from './repositories/transactions/create-pair-transaction.body';
+import { CreatePairTransactionResult } from './repositories/transactions/create-pair-transaction.result';
 
 describe('TransactionService', () => {
   let service: TransactionService;
@@ -20,65 +19,59 @@ describe('TransactionService', () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         TransactionService,
-        TransactionsRepository,
         {
-          provide: BalanceService,
+          provide: AccountsRepository,
           useValue: {
-            getBalanceIgnoringCache: (accountId: number, userId: string) => {
-              switch (userId) {
-                case '10f88251-d181-4255-92ed-d0d874e3a166':
-                  switch (accountId) {
-                    case 1234:
-                      return 2000;
-                    case 2345:
-                      return 100;
-                  }
-                }
-              throw new ForbiddenException();
-            },
+            accountExists: (accountId: number, isActive = false): boolean => {
+              let status = true;
+
+              switch(accountId) {
+                case 1:
+                  status = false;
+                  break;
+                case 2:
+                  status = true;
+                  break;
+                case 3:
+                  status = false;
+                  break;
+              }
+
+              return status;
+            }
           },
         },
         {
-          provide: BalancesRepository,
-          useValue: {},
+          provide: BalanceService,
+          useValue: {
+            getBalanceIgnoringCache: (accountId: number, userId: string): number => {
+              let balance = 0;
+
+              switch (accountId) {
+                case 5:
+                  balance = 1000;
+                  break;
+              }
+
+              return balance;
+            }
+          }
+        },
+        {
+          provide: TransactionsRepository,
+          useValue: {
+            createPairTransaction: (transaction: CreatePairTransactionBody): CreatePairTransactionResult => {
+              return {
+                fromTransactionId: 1234,
+                toTransactionId: 5678,
+              };
+            }
+          }
         },
         {
           provide: CACHE_MANAGER,
           useValue: {
             del: (key) => {},
-          },
-        },
-        {
-          provide: getRepositoryToken(TransactionEntity),
-          useValue: {
-            save: (entity: TransactionEntity) => {
-              switch (entity.account.accountId) {
-                case 1234:
-                  entity.transactionId = 42;
-                  return entity;
-                default:
-                  throw new Error('insert or update on table error');
-              }
-            },
-          },
-        },
-        {
-          provide: UserService,
-          useValue: {
-            hasAccessToAccount: (userId: string, accountId: number) => {
-              switch (userId) {
-                case '10f88251-d181-4255-92ed-d0d874e3a166':
-                  switch (accountId) {
-                    case 1234:
-                    case 2345:
-                      return true;
-                    default:
-                      return false;
-                  }
-                default:
-                  return true;
-              }
-            },
           },
         },
       ],
@@ -91,99 +84,106 @@ describe('TransactionService', () => {
     expect(service).toBeDefined();
   });
 
-  describe('transaction.service -> createTransaction()', () => {
-    describe('account ownership and existence', () => {
-      describe('debit', () => {
-        test('user does not have access rights to the account', async () => {
-          try {
-            await service.createTransaction('10f88251-d181-4255-92ed-d0d874e3a166', {
-              accountId: 4242,
-              type: TransactionTypeEnum.DEBIT,
-              amount: -1200,
-            });
-          } catch (error) {
-            expect(error).toBeInstanceOf(ForbiddenException);
-          }
-        });
-    
-        test('account does not exists', async () => {
-          try {
-            await service.createTransaction('10f88251-d181-4255-92ed-d0d874e3a166', {
-              accountId: 9876,
-              type: TransactionTypeEnum.DEBIT,
-              amount: -1200,
-            });
-          } catch (error) {
-            expect(error).toBeInstanceOf(ForbiddenException);
-          }
-        });
-      });
-
-      describe('credit', () => {
-        test('user does not have access rights to the account', async () => {
-          try {
-            await service.createTransaction('10f88251-d181-4255-92ed-d0d874e3a166', {
-              accountId: 4242,
-              type: TransactionTypeEnum.CREDIT,
-              amount: 1200,
-            });
-          } catch (error) {
-            expect(error).toBeInstanceOf(ForbiddenException);
-          }
-        });
-    
-        test('account does not exists', async () => {
-          try {
-            await service.createTransaction('10f88251-d181-4255-92ed-d0d874e3a166', {
-              accountId: 9876,
-              type: TransactionTypeEnum.CREDIT,
-              amount: 1200,
-            });
-          } catch (error) {
-            expect(error).toBeInstanceOf(ForbiddenException);
-          }
-        });
+  describe('transaction.service -> createDebitTransaction()', () => {
+    describe('transaction type errors', () => {
+      test('transaction type is not a debit', async () => {
+        try {
+          await service.createDebitTransaction('', {
+            accountId: 0,
+            pairAccountId: 0,
+            type: TransactionTypeEnum.CREDIT,
+            amount: 0,
+          });
+        } catch (error) {
+          expect(error).toBeInstanceOf(PreconditionFailedException);
+          expect(error.response.data.name).toBe('InvalidTransactionType');
+          expect(error.response.data.errors).toEqual(expect.arrayContaining(['only debit transaction type allowed']));
+        }
       });
     });
 
-    describe('transaction creation', () => {
-      describe('debit', () => {
-        test('account with insufficient balance', async () => {
-          try {
-            await service.createTransaction('10f88251-d181-4255-92ed-d0d874e3a166', {
-              accountId: 2345,
-              type: TransactionTypeEnum.DEBIT,
-              amount: -200,
-            });
-          } catch (error) {
-            expect(error).toBeInstanceOf(PreconditionFailedException);
-          }
-        });
-    
-        test('transaction created without errors', async () => {
-          const result = await service.createTransaction('10f88251-d181-4255-92ed-d0d874e3a166', {
-            accountId: 1234,
+    describe('account errors', () => {
+      test('accountId does not exist or is inactive', async () => {
+        try {
+          await service.createDebitTransaction('', {
+            accountId: 1,
+            pairAccountId: 2,
             type: TransactionTypeEnum.DEBIT,
-            amount: -1200,
+            amount: 0,
           });
-    
-          expect(result).toHaveProperty('transactionId');
-          expect(result.transactionId).toBe(42);
-        });
+        } catch (error) {
+          expect(error).toBeInstanceOf(ForbiddenException);
+          expect(error.response.data.name).toBe('InexistentOrInactiveAccount');
+          expect(error.response.data.errors).toEqual(expect.arrayContaining(['accountId does not exists or it is inactive']));
+          expect(error.response.data.errors).toEqual(expect.not.arrayContaining(['pairAccountId does not exists or it is inactive']));
+        }
       });
 
-      describe('credit', () => {
-        test('transaction created without errors', async () => {
-          const result = await service.createTransaction('10f88251-d181-4255-92ed-d0d874e3a166', {
-            accountId: 1234,
-            type: TransactionTypeEnum.CREDIT,
-            amount: 1200,
+      test('pairAccountId does not exist or is inactive', async () => {
+        try {
+          await service.createDebitTransaction('', {
+            accountId: 2,
+            pairAccountId: 1,
+            type: TransactionTypeEnum.DEBIT,
+            amount: 0,
           });
-    
-          expect(result).toHaveProperty('transactionId');
-          expect(result.transactionId).toBe(42);
+        } catch (error) {
+          expect(error).toBeInstanceOf(ForbiddenException);
+          expect(error.response.data.name).toBe('InexistentOrInactiveAccount');
+          expect(error.response.data.errors).toEqual(expect.not.arrayContaining(['accountId does not exists or it is inactive']));
+          expect(error.response.data.errors).toEqual(expect.arrayContaining(['pairAccountId does not exists or it is inactive']));
+        }
+      });
+
+      test('accountId and pairAccountId does not exist or is inactive', async () => {
+        try {
+          await service.createDebitTransaction('', {
+            accountId: 1,
+            pairAccountId: 3,
+            type: TransactionTypeEnum.DEBIT,
+            amount: 0,
+          });
+        } catch (error) {
+          expect(error).toBeInstanceOf(ForbiddenException);
+          expect(error.response.data.name).toBe('InexistentOrInactiveAccount');
+          expect(error.response.data.errors).toEqual(expect.arrayContaining(['accountId does not exists or it is inactive']));
+          expect(error.response.data.errors).toEqual(expect.arrayContaining(['pairAccountId does not exists or it is inactive']));
+        }
+      });
+    });
+
+    describe('balance errors', () => {
+      test('accountId has insufficient balance', async () => {
+        try {
+          await service.createDebitTransaction('', {
+            accountId: 4,
+            pairAccountId: 5,
+            type: TransactionTypeEnum.DEBIT,
+            amount: -200,
+          });
+        } catch (error) {
+          expect(error).toBeInstanceOf(PreconditionFailedException);
+          expect(error.response.data.name).toBe('InsufficientAccountBalance');
+          expect(error.response.data).not.toHaveProperty('errors');
+        }
+      });
+    });
+
+    describe('successful transactions', () => {
+      test('transaction created without errors', async () => {
+        const result = await service.createDebitTransaction('', {
+          accountId: 5,
+          pairAccountId: 6,
+          type: TransactionTypeEnum.DEBIT,
+          amount: -200,
         });
+
+        expect(result).toHaveProperty('fromTransactionId');
+        expect(result).toHaveProperty('toTransactionId');
+        expect(result.fromTransactionId).toBe(1234);
+        expect(result.toTransactionId).toBe(5678);
       });
     });
   });
+
 });
