b9411754fb65c53ea29b24a09d2b84249fadf96f
shimisnow <14943507+shimisnow@users.noreply.github.com>
2024-02-20 09:17:52 -0300

    feat(#73): implements createPairTransaction() function at the transactions repository

---
+++ apps/financial-service/src/app/transaction/repositories/transactions/create-pair-transaction.body.ts
@@ -0,0 +1,24 @@
+import { TransactionTypeEnum } from '@shared/database/financial/enums/transaction-type.enum';
+
+/** Defines the information needed to create a pair transaction */
+export class CreatePairTransactionBody {
+  /** Transaction one */ 
+  from: {
+    /** Account to be linked with this transaction */
+    accountId: number;
+    /** Recommended that 'from' and 'to' to be different types */
+    type: TransactionTypeEnum;
+    /** Recommended to be negative value for debit transaction */
+    amount: number;
+  };
+
+  /** Transaction two */ 
+  to: {
+    /** Account to be linked with this transaction */
+    accountId: number;
+    /** Recommended that 'from' and 'to' to be different types */
+    type: TransactionTypeEnum;
+    /** Recommended to be negative value for debit transaction */
+    amount: number;
+  };
+}
---
+++ apps/financial-service/src/app/transaction/repositories/transactions/create-pair-transaction.result.ts
@@ -0,0 +1,7 @@
+/** Information about the created and linked transactions */
+export class CreatePairTransactionResult {
+  /** Id for the created transaction with data from the 'from' input  */
+  fromTransactionId: number;
+  /** Id for the created transaction with data from the 'to' input */
+  toTransactionId: number;
+}
