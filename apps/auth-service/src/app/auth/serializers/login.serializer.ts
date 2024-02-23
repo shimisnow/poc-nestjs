bbcf8f27f7098867bd4e2e3bc18f79cd39e3f29f
shimisnow <14943507+shimisnow@users.noreply.github.com>
2024-02-23 14:45:48 -0300

    feat(#75): make refresh token option at login endpoint

--- apps/auth-service/src/app/auth/serializers/login.serializer.ts
+++ apps/auth-service/src/app/auth/serializers/login.serializer.ts
@@ -10,8 +10,9 @@ export class LoginSerializer {
 
   @ApiProperty({
     description: 'Refresh token to be used to get new access token',
+    required: false,
     example:
       'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFuZGVyc29uIiwic3ViIjoxLCJpYXQiOjE2ODM4MzAyNTEsImV4cCI6MTY4MzgzMDMxMX0.eN5Cv2tJ0HGlVNKMtPv5VPeCIA7dd4OEA-8Heh7OJ_c',
   })
-  refreshToken: string;
+  refreshToken?: string;
 }
