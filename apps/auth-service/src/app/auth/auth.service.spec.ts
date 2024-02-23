ed888a83967a663611bc3a82c752921a265ae62d
shimisnow <14943507+shimisnow@users.noreply.github.com>
2024-02-23 14:49:04 -0300

    test(#75): unit test

--- apps/auth-service/src/app/auth/auth.service.spec.ts
+++ apps/auth-service/src/app/auth/auth.service.spec.ts
@@ -92,8 +92,8 @@ describe('AuthService', () => {
   });
 
   describe('auth.service -> login()', () => {
-    test('correct login data with ACTIVE user', async () => {
-      const result = await service.login('anderson', 'test@1234');
+    test('correct login data with ACTIVE user (with refresh)', async () => {
+      const result = await service.login('anderson', 'test@1234', true);
 
       expect(result).toHaveProperty('accessToken');
       expect(result).toHaveProperty('refreshToken');
@@ -111,9 +111,22 @@ describe('AuthService', () => {
       expect(parseInt(refreshToken.loginId)).toBeLessThan(new Date().getTime());
     });
 
+    test('correct login data with ACTIVE user (without refresh)', async () => {
+      const result = await service.login('anderson', 'test@1234', false);
+
+      expect(result).toHaveProperty('accessToken');
+      expect(result).not.toHaveProperty('refreshToken');
+      
+      const accessToken = jsonwebtoken.verify(result.accessToken, process.env.JWT_SECRET_KEY) as JwtPayload;
+      expect(accessToken).toHaveProperty('userId');
+      expect(accessToken.userId).toBe('4b3c74ae-57aa-4752-9452-ed083b6d4bfa');
+      expect(accessToken).toHaveProperty('loginId');
+      expect(parseInt(accessToken.loginId)).toBeLessThan(new Date().getTime());
+    });
+
     test('correct login data with INACTIVE user', async () => {
       try {
-        await service.login('thomas', 'test@1234');
+        await service.login('thomas', 'test@1234', true);
       } catch (error) {
         expect(error).toBeInstanceOf(UnauthorizedException);
       }
@@ -121,7 +134,7 @@ describe('AuthService', () => {
 
     test('incorrect login data (user exists)', async () => {
       try {
-        await service.login('anderson', 'test@5678');
+        await service.login('anderson', 'test@5678', true);
       } catch (error) {
         expect(error).toBeInstanceOf(UnauthorizedException);
       }
@@ -129,7 +142,7 @@ describe('AuthService', () => {
 
     test('incorrect login data (user does not exists)', async () => {
       try {
-        await service.login('beatrice', 'test@1234');
+        await service.login('beatrice', 'test@1234', true);
       } catch (error) {
         expect(error).toBeInstanceOf(UnauthorizedException);
       }
@@ -137,7 +150,7 @@ describe('AuthService', () => {
 
     test('some database error', async () => {
       try {
-        await service.login('anything', 'test@1234');
+        await service.login('anything', 'test@1234', true);
       } catch (error) {
         expect(error).toBeInstanceOf(BadGatewayException);
       }
