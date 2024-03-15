d0b1d64e1c927dd9c52b031be40bcae1d51bd26a
shimisnow <14943507+shimisnow@users.noreply.github.com>
2024-03-15 16:05:27 -0300

    feat[#104]: add auth guard to country resolver

--- apps/user-service/src/app/countries/countries.resolver.ts
+++ apps/user-service/src/app/countries/countries.resolver.ts
@@ -3,12 +3,15 @@ import { CountryModel } from './models/country.model';
 import { CountriesService } from './countries.service';
 import { CountryCodeEnum } from '../database/enums/country-code.enum';
 import { GraphQLQueryFields } from '../utils/decorators/graphql-query-fields.decorator';
+import { UseGuards } from '@nestjs/common';
+import { GraphQLAuthGuard } from '@shared/authentication/graphql';
 
 @Resolver(() => CountryModel)
 export class CountriesResolver {
   constructor(private countriesService: CountriesService) {}
 
   @Query(() => CountryModel, { name: 'country' })
+  @UseGuards(GraphQLAuthGuard)
   async getCountry(
     @Args('code', { type: () => CountryCodeEnum })
     code: CountryCodeEnum,
