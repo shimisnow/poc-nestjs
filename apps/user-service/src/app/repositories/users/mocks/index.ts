a936b8ccda8ac1ae4e2df0c6fc0fb67e6561e019
shimisnow <14943507+shimisnow@users.noreply.github.com>
2024-03-16 11:21:28 -0300

    test[#95]: add entity mocks for user

--- apps/user-service/src/app/repositories/users/mocks/index.ts
+++ apps/user-service/src/app/repositories/users/mocks/index.ts
@@ -1,5 +1,11 @@
 import { UserOneEntityMock } from './user-1-entity.mock';
 import { UserTwoEntityMock } from './user-2-entity.mock';
 import { UserThreeEntityMock } from './user-3-entity.mock';
+import { UsersRepositoryMock } from './users-repository.mock';
 
-export { UserOneEntityMock, UserTwoEntityMock, UserThreeEntityMock };
+export {
+  UsersRepositoryMock,
+  UserOneEntityMock,
+  UserTwoEntityMock,
+  UserThreeEntityMock,
+};
