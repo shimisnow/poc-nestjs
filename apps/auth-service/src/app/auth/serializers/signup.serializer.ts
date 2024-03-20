c8fdeff025c1e0037a653ecb6115bf69f97b0bc8
shimisnow <14943507+shimisnow@users.noreply.github.com>
2024-03-20 10:47:51 -0300

    feat[#114]: remove userId from dto and transfer it to serializer

--- apps/auth-service/src/app/auth/serializers/signup.serializer.ts
+++ apps/auth-service/src/app/auth/serializers/signup.serializer.ts
@@ -1,3 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class SignUpSerializer {
   status: boolean;
+
+  @ApiProperty({
+    description: 'Generated user unique ID to be used at all services',
+    required: false,
+    example: 'cf0accda-2ace-415b-8b02-5bd8bf267a9c',
+  })
+  userId?: string;
 }
