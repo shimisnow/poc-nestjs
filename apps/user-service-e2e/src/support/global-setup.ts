d6390707c608668a39a7164e48d279117f37800c
shimisnow <14943507+shimisnow@users.noreply.github.com>
2024-10-31 10:54:59 -0300

    tests[#130]: isolate e2e for user-service

--- apps/user-service-e2e/src/support/global-setup.ts
+++ apps/user-service-e2e/src/support/global-setup.ts
@@ -10,13 +10,41 @@ import { PostgreSqlContainer } from '@testcontainers/postgresql';
 import { RedisContainer } from '@testcontainers/redis';
 
 module.exports = async function () {
-  const DOCKER_IMAGE_BUILD_NAME = 'poc-nestjs-node';
+  const DOCKER_IMAGE_POCNESTJS_BASE_DEV = 'pocnestjs-base-dev';
+  const DOCKER_IMAGE_POCNESTJS_BASE_PROD = 'pocnestjs-base-prod';
+  const DOCKER_IMAGE_USER_SERVICE = 'shimisnow/pocnestjs-auth-service:latest';
   const DOCKER_POSTGRES_TAG = 'postgres:16.1';
   const DOCKER_REDIS_TAG = 'redis:7.2.4';
 
   const dockerNetwork: StartedNetwork = await new Network().start();
 
-  /***** DATABASE *****/
+  /***** BUILD BASE DOCKER IMAGES *****/
+
+  const [baseImageDev, baseImageProd] = await Promise.all([
+    // build docker image with node_module for development
+    GenericContainer.fromDockerfile('./')
+      .withBuildArgs({ NODE_ENV: 'development' })
+      .build(DOCKER_IMAGE_POCNESTJS_BASE_DEV, {
+        deleteOnExit: false,
+      }),
+    // build docker image with node_module for production
+    GenericContainer.fromDockerfile('./')
+      .withBuildArgs({ NODE_ENV: 'production' })
+      .build(DOCKER_IMAGE_POCNESTJS_BASE_PROD, {
+        deleteOnExit: false,
+      }),
+  ]);
+
+  /***** BUILD SERVICE DOCKER IMAGE *****/
+
+  const serviceImage = await GenericContainer.fromDockerfile(
+    './',
+    'apps/user-service/Dockerfile',
+  ).build(DOCKER_IMAGE_USER_SERVICE, {
+    deleteOnExit: true,
+  });
+
+  /***** DEPENDENCIES SETUP *****/
 
   const postgreSqlContainer = new PostgreSqlContainer(DOCKER_POSTGRES_TAG)
     .withNetwork(dockerNetwork)
@@ -37,42 +65,26 @@ module.exports = async function () {
       ),
     );
 
-  /***** CACHE *****/
-
   const redisContainer = new RedisContainer(DOCKER_REDIS_TAG)
     .withLabels({ 'poc-nestjs-name': 'user-service-cache' })
     .withNetwork(dockerNetwork)
     .withNetworkAliases('redis')
     .withExposedPorts(parseInt(process.env.REDIS_PORT));
 
-  /***** DEPENDENCIES START AND CODE BUILD *****/
+  /***** DEPENDENCIES START *****/
 
-  const [containerDatabase, containerCache, buildContainerCode] =
-    await Promise.all([
-      postgreSqlContainer.start(),
-      redisContainer.start(),
-      // build docker image with compiled code
-      GenericContainer.fromDockerfile('./')
-        .withBuildArgs({ NODE_ENV: 'production' })
-        .build(DOCKER_IMAGE_BUILD_NAME, {
-          deleteOnExit: false,
-        }),
-    ]);
+  const [containerDatabase, containerCache] = await Promise.all([
+    postgreSqlContainer.start(),
+    redisContainer.start(),
+  ]);
 
   /***** CODE *****/
 
-  const containerCode: StartedTestContainer = await buildContainerCode
+  const containerCode: StartedTestContainer = await serviceImage
     .withLabels({ 'poc-nestjs-name': 'user-service-code' })
     .withNetwork(dockerNetwork)
     .withNetworkAliases('user-service')
     .withExposedPorts(parseInt(process.env.USER_SERVICE_PORT))
-    .withCopyFilesToContainer([
-      {
-        source: './dist/apps/user-service/main.js',
-        target: '/home/node/app/main.js',
-      },
-    ])
-    .withCommand(['node', 'main.js'])
     .withEnvironment({
       DATABASE_USER_HOST: 'database-info',
       DATABASE_USER_PORT: '5432',
