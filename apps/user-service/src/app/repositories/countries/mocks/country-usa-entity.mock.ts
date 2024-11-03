c88cc45fcb014bb2bbe85052ec140237f6001170
shimisnow <14943507+shimisnow@users.noreply.github.com>
2024-11-03 11:30:03 -0300

    revert[#133]: removes user-service and user-service-e2e

--- apps/user-service/src/app/repositories/countries/mocks/country-usa-entity.mock.ts
+++
@@ -1,8 +0,0 @@
-import { CountryCodeEnum } from '../../../database/enums/country-code.enum';
-
-export const CountryUsaEntityMock = {
-  code: CountryCodeEnum.USA,
-  callingCode: 1,
-  createdAt: new Date(),
-  updatedAt: new Date(),
-};
--- apps/user-service/src/app/repositories/users/mocks/user-3-entity.mock.ts
+++
@@ -1,8 +0,0 @@
-export const UserThreeEntityMock = {
-  userId: 'c136d3ce-7380-4fd4-9199-429a6d687083',
-  name: 'Three',
-  surname: 'Tres',
-  email: 'three.tres@email.com',
-  createdAt: new Date(),
-  updatedAt: new Date(),
-};
