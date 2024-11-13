31e3372920b521a5963902f6308a0cee63404266
shimisnow <14943507+shimisnow@users.noreply.github.com>
2024-11-13 14:35:53 -0300

    docs[#148]: update openapi docs for auth-service

--- apps/auth-service/src/app/auth/serializers/verify-token-invalidation-process.serializer.ts
+++ apps/auth-service/src/app/auth/serializers/verify-token-invalidation-process.serializer.ts
@@ -1,6 +1,18 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { InvalidatedErrorEnum } from '../enums/invalidated-error.enum';
 
 export class VerifyTokenInvalidationProcessSerializer {
+  @ApiProperty({
+    description: 'Information if is valid against the invalidation process',
+    example: false,
+  })
   valid: boolean;
+
+  @ApiProperty({
+    description: 'Motive because the token is invalid',
+    type: 'enum',
+    enum: InvalidatedErrorEnum,
+    example: InvalidatedErrorEnum.INVALIDATED_BY_LOGOUT,
+  })
   invalidatedBy?: InvalidatedErrorEnum;
 }
