96194be3d4867f38f30eec6a5ffca504dd088910
shimisnow <14943507+shimisnow@users.noreply.github.com>
2024-11-13 10:37:18 -0300

    feat[#143]: add guard module

--- apps/financial-service/src/app/balance/balance.module.ts
+++ apps/financial-service/src/app/balance/balance.module.ts
@@ -3,9 +3,10 @@ import { BalanceService } from './balance.service';
 import { BalanceController } from './balance.controller';
 import { BalancesRepositoryModule } from './repositories/balances-repository.module';
 import { UserModule } from '../user/user.module';
+import { AuthGuardModule } from '@shared/authentication/guards/auth-guard.module';
 
 @Module({
-  imports: [BalancesRepositoryModule, UserModule],
+  imports: [BalancesRepositoryModule, UserModule, AuthGuardModule],
   providers: [BalanceService],
   controllers: [BalanceController],
   exports: [BalanceService],
