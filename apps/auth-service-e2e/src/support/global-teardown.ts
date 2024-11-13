f3a31e3a7d62186e1efaf11595d2e40f31974b65
shimisnow <14943507+shimisnow@users.noreply.github.com>
2024-11-13 16:37:20 -0300

    style[#148]: organize variables name

--- apps/auth-service-e2e/src/support/global-teardown.ts
+++ apps/auth-service-e2e/src/support/global-teardown.ts
@@ -1,16 +1,18 @@
+/* eslint-disable */
 import * as fs from 'fs';
 import { ensureFile } from 'fs-extra';
 
 module.exports = async function () {
-  const archivePath = './apps/auth-service-e2e/logs/auth-service-logs.tar';
-  await ensureFile(archivePath);
-  const outputFileStream = fs.createWriteStream(archivePath);
+  /***** Auth Log *****/
+  const authArchivePath = './apps/auth-service-e2e/logs/auth-service-logs.tar';
+  await ensureFile(authArchivePath);
+  const authOutputFileStream = fs.createWriteStream(authArchivePath);
 
-  const homeNodeAppArquive: NodeJS.ReadableStream =
-    await globalThis.containerCode.copyArchiveFromContainer(
+  const authHomeNodeAppArquive: NodeJS.ReadableStream =
+    await globalThis.authCodeContainer.copyArchiveFromContainer(
       '/home/node/app/logs',
     );
-  homeNodeAppArquive.pipe(outputFileStream);
+  authHomeNodeAppArquive.pipe(authOutputFileStream);
 
   await Promise.allSettled([
     globalThis.authDatabaseContainer.stop(),
--- apps/financial-service-e2e/src/support/global-teardown.ts
+++ apps/financial-service-e2e/src/support/global-teardown.ts
@@ -1,23 +1,8 @@
-import * as fs from 'fs';
-import { ensureFile } from 'fs-extra';
-
+/* eslint-disable */
 module.exports = async function () {
-  const archivePath =
-    './apps/financial-service-e2e/logs/financial-service-logs.tar';
-  await ensureFile(archivePath);
-  const outputFileStream = fs.createWriteStream(archivePath);
-
-  const homeNodeAppArquive: NodeJS.ReadableStream =
-    await globalThis.containerCode.copyArchiveFromContainer(
-      '/home/node/app/logs',
-    );
-  homeNodeAppArquive.pipe(outputFileStream);
-
   await Promise.allSettled([
     globalThis.authDatabaseContainer.stop(),
     globalThis.financialDatabaseContainer.stop(),
-    globalThis.cacheContainer.stop(),
     globalThis.authCodeContainer.stop(),
-    globalThis.financialCodeContainer.stop(),
   ]);
 };
