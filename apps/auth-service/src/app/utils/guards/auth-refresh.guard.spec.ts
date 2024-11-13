20262647d03b7ab1ca2c6197bb9b0ca2d702d105
shimisnow <14943507+shimisnow@users.noreply.github.com>
2024-11-13 10:35:45 -0300

    refactor[#143]: make auth-service implement its own guard. Does not uses shared guard

---
+++ apps/auth-service/src/app/utils/guards/auth-refresh.guard.spec.ts
@@ -0,0 +1,279 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Test, TestingModule } from '@nestjs/testing';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { JwtService } from '@nestjs/jwt';
+import * as jsonwebtoken from 'jsonwebtoken';
+import { AuthRefreshGuard } from './auth-refresh.guard';
+import { CacheManagerMock } from './mocks/cache-manager.mock';
+import { UnauthorizedException } from '@nestjs/common';
+import { AuthErrorNames } from '@shared/authentication/enums/auth-error-names.enum';
+import { AuthErrorMessages } from '@shared/authentication/enums/auth-error-messages.enum';
+import { UserPayload } from '@shared/authentication/payloads/user.payload';
+
+const generateContext = (authToken: string) => {
+  const context = {
+    switchToHttp: () => {
+      return {
+        getRequest: () => {
+          return {
+            headers: {
+              authorization: `Bearer ${authToken}`,
+            },
+          };
+        },
+      };
+    },
+  };
+
+  return context;
+};
+
+describe('auth-refresh.guard', () => {
+  let guard: AuthRefreshGuard;
+  const JWT_REFRESH_SECRET_KEY = process.env.JWT_REFRESH_SECRET_KEY;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        AuthRefreshGuard,
+        JwtService,
+        {
+          provide: CACHE_MANAGER,
+          useClass: CacheManagerMock,
+        },
+      ],
+    }).compile();
+
+    guard = module.get<AuthRefreshGuard>(AuthRefreshGuard);
+  });
+
+  describe('canActivate()', () => {
+    test('empty token', async () => {
+      try {
+        await guard.canActivate(generateContext('') as any);
+      } catch (error) {
+        expect(error).toBeInstanceOf(UnauthorizedException);
+        const response = error.response;
+        expect(response).toHaveProperty('data');
+        expect(response.data.name).toBe(AuthErrorNames.JWT_EMPTY_ERROR);
+      }
+    });
+
+    test('invalid jwt token', async () => {
+      try {
+        await guard.canActivate(
+          generateContext('abcdefghijklmnopqrstuvwxyz') as any,
+        );
+      } catch (error) {
+        expect(error).toBeInstanceOf(UnauthorizedException);
+        const response = error.response;
+        expect(response).toHaveProperty('data');
+        expect(response.data.name).toBe(AuthErrorNames.JWT_ERROR);
+        expect(response.data.errors).toEqual(
+          expect.arrayContaining(['jwt malformed']),
+        );
+      }
+    });
+
+    test('invalid jwt token signature', async () => {
+      const now = Math.floor(Date.now() / 1000);
+      const token = jsonwebtoken.sign(
+        {
+          userId: '4799cc31-7692-40b3-afff-cc562baf5374',
+          loginId: new Date().getTime().toString(),
+          role: 'user',
+          iat: now,
+          exp: now + 60,
+        } as UserPayload,
+        'fake-key',
+      );
+
+      try {
+        await guard.canActivate(generateContext(token) as any);
+      } catch (error) {
+        expect(error).toBeInstanceOf(UnauthorizedException);
+        const response = error.response;
+        expect(response).toHaveProperty('data');
+        expect(response.data.name).toBe(AuthErrorNames.JWT_ERROR);
+        expect(response.data.errors).toEqual(
+          expect.arrayContaining(['invalid signature']),
+        );
+      }
+    });
+
+    test('payload with incorrect structure', async () => {
+      const now = Math.floor(Date.now() / 1000);
+      const token = jsonwebtoken.sign(
+        {
+          userId: '4799cc31-7692-40b3-afff-cc562baf5374',
+          iat: now,
+          exp: now + 60,
+        } as UserPayload,
+        JWT_REFRESH_SECRET_KEY,
+      );
+
+      try {
+        await guard.canActivate(generateContext(token) as any);
+      } catch (error) {
+        expect(error).toBeInstanceOf(UnauthorizedException);
+        const response = error.response;
+        expect(response).toHaveProperty('data');
+        expect(response.data.name).toBe(
+          AuthErrorNames.JWT_PAYLOAD_STRUCTURE_ERROR,
+        );
+        expect(response.data.errors).toEqual(
+          expect.arrayContaining(['loginId must be a number string']),
+        );
+      }
+    });
+
+    test('invalidated by user logout', async () => {
+      const now = Math.floor(Date.now() / 1000);
+      const token = jsonwebtoken.sign(
+        {
+          // user in cache for logout
+          userId: '01e57c05-45d6-4d6f-8f30-2bddce37df5f',
+          loginId: '1708003432088',
+          role: 'user',
+          iat: now,
+          exp: now + 60,
+        } as UserPayload,
+        JWT_REFRESH_SECRET_KEY,
+      );
+
+      try {
+        await guard.canActivate(generateContext(token) as any);
+      } catch (error) {
+        expect(error).toBeInstanceOf(UnauthorizedException);
+        const response = error.response;
+        expect(response).toHaveProperty('data');
+        expect(response.data.name).toBe(
+          AuthErrorNames.JWT_INVALIDATED_BY_SERVER,
+        );
+        expect(response.data.errors).toEqual(
+          expect.arrayContaining([AuthErrorMessages.INVALIDATED_BY_LOGOUT]),
+        );
+      }
+    });
+
+    test('invalidated by user password change', async () => {
+      const now = Math.floor(Date.now() / 1000);
+      const token = jsonwebtoken.sign(
+        {
+          // user in cache for password change
+          userId: '3e699250-4bc4-4c3d-a0ea-0aa3dc17abd5',
+          loginId: new Date().getTime().toString(),
+          role: 'user',
+          iat: Math.floor((new Date().getTime() - 10000) / 1000),
+          exp: now + 60,
+        } as UserPayload,
+        JWT_REFRESH_SECRET_KEY,
+      );
+
+      try {
+        await guard.canActivate(generateContext(token) as any);
+      } catch (error) {
+        expect(error).toBeInstanceOf(UnauthorizedException);
+        const response = error.response;
+        expect(response).toHaveProperty('data');
+        expect(response.data.name).toBe(
+          AuthErrorNames.JWT_INVALIDATED_BY_SERVER,
+        );
+        expect(response.data.errors).toEqual(
+          expect.arrayContaining([
+            AuthErrorMessages.INVALIDATED_BY_PASSWORD_CHANGE,
+          ]),
+        );
+      }
+    });
+
+    test('token issued after password change', async () => {
+      const now = Math.floor(Date.now() / 1000);
+      const token = jsonwebtoken.sign(
+        {
+          // user in cache for password change
+          userId: '3e699250-4bc4-4c3d-a0ea-0aa3dc17abd5',
+          loginId: new Date().getTime().toString(),
+          role: 'user',
+          iat: now,
+          exp: now + 60,
+        } as UserPayload,
+        JWT_REFRESH_SECRET_KEY,
+      );
+
+      const result = await guard.canActivate(generateContext(token) as any);
+      expect(result).toBeTruthy();
+    });
+
+    test('token without events', async () => {
+      const now = Math.floor(Date.now() / 1000);
+      const token = jsonwebtoken.sign(
+        {
+          // user not in cache
+          userId: '68ec369c-30f3-4e56-b286-290cde1efd7c',
+          loginId: new Date().getTime().toString(),
+          role: 'user',
+          iat: now,
+          exp: now + 60,
+        } as UserPayload,
+        JWT_REFRESH_SECRET_KEY,
+      );
+
+      const result = await guard.canActivate(generateContext(token) as any);
+      expect(result).toBeTruthy();
+    });
+  });
+
+  describe('extractTokenFromHeader()', () => {
+    // necessary to access the private mothod
+    const guard = new AuthRefreshGuard(null, null) as any;
+
+    test('empty authorization header', async () => {
+      const authHeader = {
+        headers: {
+          authorization: '',
+        },
+      };
+
+      const result = guard.extractTokenFromHeader(authHeader);
+
+      expect(result).toBeUndefined();
+    });
+
+    test('authorization header different than Bearer', async () => {
+      const authHeader = {
+        headers: {
+          authorization: 'Header eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9',
+        },
+      };
+
+      const result = guard.extractTokenFromHeader(authHeader);
+
+      expect(result).toBeUndefined();
+    });
+
+    test('Bearer authorization header with empty token', async () => {
+      const authHeader = {
+        headers: {
+          authorization: 'Bearer',
+        },
+      };
+
+      const result = guard.extractTokenFromHeader(authHeader);
+
+      expect(result).toBeUndefined();
+    });
+
+    test('correct authorization header', async () => {
+      const authHeader = {
+        headers: {
+          authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9',
+        },
+      };
+
+      const result = guard.extractTokenFromHeader(authHeader);
+
+      expect(result).toBe('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9');
+    });
+  });
+});
---
+++ apps/auth-service/src/app/utils/guards/auth-refresh.guard.ts
@@ -0,0 +1,143 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  HttpStatus,
+  Inject,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Cache } from 'cache-manager';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { JwtService } from '@nestjs/jwt';
+import { Request } from 'express';
+import { plainToClass } from 'class-transformer';
+import { validateOrReject } from 'class-validator';
+import { UserPayload } from '@shared/authentication/payloads/user.payload';
+import { CacheKeyPrefix } from '@shared/cache/enums/cache-key-prefix.enum';
+import { AuthErrorMessages } from '@shared/authentication/enums/auth-error-messages.enum';
+import { AuthErrorNames } from '@shared/authentication/enums/auth-error-names.enum';
+import { PasswordChangeCachePayload } from '@shared/cache/payloads/password-change-cache.payload';
+
+@Injectable()
+export class AuthRefreshGuard implements CanActivate {
+  constructor(
+    @Inject(CACHE_MANAGER) private cacheService: Cache,
+    private jwtService: JwtService,
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const token = this.extractTokenFromHeader(request);
+
+    request['user'] = await this.extractPayloadFromJwtToken(token);
+
+    return true;
+  }
+
+  private extractTokenFromHeader(request: Request): string | undefined {
+    const [type, token] = request.headers.authorization?.split(' ') ?? [];
+    return type === 'Bearer' ? token : undefined;
+  }
+
+  private async extractPayloadFromJwtToken(
+    token: string,
+  ): Promise<UserPayload> {
+    if (!token) {
+      throw new UnauthorizedException({
+        statusCode: HttpStatus.UNAUTHORIZED,
+        data: {
+          name: AuthErrorNames.JWT_EMPTY_ERROR,
+        },
+      });
+    }
+
+    let payload: UserPayload;
+
+    // JWT verification
+    try {
+      payload = await this.jwtService.verifyAsync(token, {
+        secret: process.env.JWT_REFRESH_SECRET_KEY,
+        maxAge: process.env.JWT_REFRESH_MAX_AGE ?? '1h',
+      });
+    } catch (error) {
+      const exceptionBody = {
+        statusCode: HttpStatus.UNAUTHORIZED,
+        data: error,
+      };
+
+      // if there is a message at the body, change it to the errors key
+      // done for the sake of pattern
+      if (exceptionBody.data?.message) {
+        exceptionBody.data.errors = [exceptionBody.data?.message];
+
+        delete exceptionBody.data['message'];
+      }
+
+      throw new UnauthorizedException(exceptionBody);
+    }
+
+    // verifies if the payload has the correct structure
+    try {
+      payload = plainToClass(UserPayload, payload);
+      await validateOrReject(payload);
+    } catch (errors) {
+      const messages = [];
+
+      errors.forEach((error) => {
+        Object.keys(error.constraints).forEach((key) => {
+          messages.push(error.constraints[key]);
+        });
+      });
+
+      throw new UnauthorizedException({
+        statusCode: HttpStatus.UNAUTHORIZED,
+        data: {
+          name: AuthErrorNames.JWT_PAYLOAD_STRUCTURE_ERROR,
+          errors: messages,
+        },
+      });
+    }
+
+    // verify if the combination of userId with loginId is marked in cache as invalid
+    const logoutVerification = await this.cacheService.get(
+      [
+        CacheKeyPrefix.AUTH_SESSION_LOGOUT,
+        payload.userId,
+        payload.loginId,
+      ].join(':'),
+    );
+
+    if (logoutVerification !== null) {
+      throw new UnauthorizedException({
+        statusCode: HttpStatus.UNAUTHORIZED,
+        data: {
+          name: AuthErrorNames.JWT_INVALIDATED_BY_SERVER,
+          errors: [AuthErrorMessages.INVALIDATED_BY_LOGOUT],
+        },
+      });
+    }
+
+    // verify if the user had a password change event
+    const passwordChangeVerification =
+      await this.cacheService.get<PasswordChangeCachePayload>(
+        [CacheKeyPrefix.AUTH_PASSWORD_CHANGE, payload.userId].join(':'),
+      );
+
+    // if there is an cache entry
+    if (passwordChangeVerification != null) {
+      // if this token was not issued after the password change
+      // iat is in seconds and changedAt at milliseconds
+      if (payload.iat * 1000 <= passwordChangeVerification.changedAt) {
+        throw new UnauthorizedException({
+          statusCode: HttpStatus.UNAUTHORIZED,
+          data: {
+            name: AuthErrorNames.JWT_INVALIDATED_BY_SERVER,
+            errors: [AuthErrorMessages.INVALIDATED_BY_PASSWORD_CHANGE],
+          },
+        });
+      }
+    }
+
+    return payload;
+  }
+}
