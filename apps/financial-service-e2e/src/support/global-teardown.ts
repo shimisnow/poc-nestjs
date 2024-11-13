ee5cf43d4f316c00ec2ae5f18abb86f85841046a
shimisnow <14943507+shimisnow@users.noreply.github.com>
2024-11-13 16:23:23 -0300

    tests[#148]: setup log save for financial-service-e2e

--- apps/financial-service-e2e/src/support/global-teardown.ts
+++ apps/financial-service-e2e/src/support/global-teardown.ts
@@ -1,4 +1,18 @@
+import * as fs from 'fs';
+import { ensureFile } from 'fs-extra';
+
 module.exports = async function () {
+  const archivePath =
+    './apps/financial-service-e2e/logs/financial-service-logs.tar';
+  await ensureFile(archivePath);
+  const outputFileStream = fs.createWriteStream(archivePath);
+
+  const homeNodeAppArquive: NodeJS.ReadableStream =
+    await globalThis.containerCode.copyArchiveFromContainer(
+      '/home/node/app/logs',
+    );
+  homeNodeAppArquive.pipe(outputFileStream);
+
   await Promise.allSettled([
     globalThis.authDatabaseContainer.stop(),
     globalThis.financialDatabaseContainer.stop(),
