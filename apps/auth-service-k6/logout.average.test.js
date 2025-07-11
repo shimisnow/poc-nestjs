abe07bf1327971f9b846b738bcb61f6012f3d380
shimisnow <14943507+shimisnow@users.noreply.github.com>
2025-07-11 13:41:58 -0300

    test[#177]: adds tests for login and logout endpoints

--- apps/auth-service-k6/logout.average.test.js
+++ apps/auth-service-k6/logout.average.test.js
@@ -1,6 +1,6 @@
 import http from 'k6/http';
 import { check, sleep } from 'k6';
-import { generateJwtHMAC } from './utils/jwt.js';
+import { generateJwtToken } from './utils/jwt.js';
 
 export const options = {
   tags: {
@@ -8,25 +8,29 @@ export const options = {
     endpoint: 'logout',
   },
   stages: [
-    { duration: '30s', target: 10 }, // traffic ramp-up from 1 to 100 users over 30 seconds.
-    //{ duration: '30s', target: 300 }, // ramp-up at 200 users for 30 seconds
-    //{ duration: '15s', target: 0 }, // ramp-down to 0 users
+    { duration: '15s', target: 10 }, // traffic ramp-up from 1 to 100 users over 15 seconds.
+    { duration: '15s', target: 300 }, // ramp-up at 200 users for 15 seconds
+    { duration: '5s', target: 0 }, // ramp-down to 0 users
   ],
   thresholds: {
     http_req_failed: ['rate<0.01'], // http errors should be less than 1%
+    'checks{response_status:200}': ['rate==1.00'], // must be 100% successful
   },
 };
 
 export default () => {
   const JWT_SECRET_KEY = '09d9059c-23c1-4309-91c7-cda91abab092';
-  const url = 'http://localhost:3001/auth/logout';
+  const host = 'http://localhost:3001';
+  const endpoint = '/auth/logout';
 
   const now = Math.floor(Date.now() / 1000);
-  const accessToken = generateJwtHMAC(
+  const accessToken = generateJwtToken(
     {
-      // changes the last three digits for the user id based on VU id
-      userId:
-        '0197f9c5-2d4b-7570-af74-11ab8181a' + String(__VU).padStart(3, '0'),
+      // changes the last twelve digits for the user id based on VU id
+      // this will generate cache entries as:
+      // auth:logout:0197f9c5-2d4b-7570-af74-000000000003:1752244098409
+      // auth:logout:0197f9c5-2d4b-7570-af74-000000000004:1752244098502
+      userId: '0197f9c5-2d4b-7570-af74-' + String(__VU).padStart(12, '0'),
       loginId: new Date().getTime().toString(),
       role: 'user',
       iat: now,
@@ -41,14 +45,12 @@ export default () => {
     Authorization: `Bearer ${accessToken}`,
   };
 
-  console.log(accessToken);
-
-  const response = http.post(url, '', { headers });
+  const response = http.post(`${host}${endpoint}`, '', { headers });
 
   check(response, {
     'status is 200': (r) => r.status === 200,
-    'response body contains performed information': (r) => {
-      const body = JSON.parse(r.body);
+    'response contains information': (r) => {
+      const body = r.json();
       if (body.hasOwnProperty('performed') === false) {
         return false;
       }
@@ -56,7 +58,7 @@ export default () => {
     },
   });
 
-  sleep(1);
+  sleep(0.25);
 };
 
 // $env:K6_WEB_DASHBOARD_EXPORT="report.html" ; k6 run --out web-dashboard .\apps\auth-service-k6\logout.test.js
--- apps/auth-service-k6/utils/jwt.js
+++ apps/auth-service-k6/utils/jwt.js
@@ -1,47 +1,29 @@
 import crypto from 'k6/crypto';
 import encoding from 'k6/encoding';
 
-const JWT_ALGORITHM = 'HS256'; // Or HS384, HS512
-
-const algToHash = {
-  HS256: 'sha256',
-  HS384: 'sha384',
-  HS512: 'sha512',
-};
-
-function base64urlEncode(str) {
-  return encoding.b64encode(str, 'rawurl');
-}
-
 function signHMAC(data, hashAlg, secret) {
   const hasher = crypto.createHMAC(hashAlg, secret);
   hasher.update(data);
-  let signature = hasher.digest('base64url'); // Should ideally be correct
-
-  // Explicitly remove padding characters if present (shouldn't be if k6 is new enough)
-  signature = signature.replace(/=+$/, '');
-
-  return signature;
+  return hasher.digest('base64url').replace(/=+$/, '');
 }
 
 /**
  * Encodes and signs a JWT with HMAC.
  * @param {Object} payload The JWT payload.
  * @param {string} secret The shared secret for HMAC.
- * @param {string} algorithm The signing algorithm (e.g., 'HS256').
  * @returns {string} The signed JWT.
  */
-export function generateJwtHMAC(payload, secret, algorithm = JWT_ALGORITHM) {
+export function generateJwtToken(payload, secret) {
   const header = {
-    alg: algorithm,
+    alg: 'HS256',
     typ: 'JWT',
   };
 
-  const encodedHeader = base64urlEncode(JSON.stringify(header));
-  const encodedPayload = base64urlEncode(JSON.stringify(payload));
+  const encodedHeader = encoding.b64encode(JSON.stringify(header), 'rawurl');
+  const encodedPayload = encoding.b64encode(JSON.stringify(payload), 'rawurl');
 
   const dataToSign = `${encodedHeader}.${encodedPayload}`;
-  const signature = signHMAC(dataToSign, algToHash[algorithm], secret);
+  const signature = signHMAC(dataToSign, 'sha256', secret);
 
   return `${encodedHeader}.${encodedPayload}.${signature}`;
 }
